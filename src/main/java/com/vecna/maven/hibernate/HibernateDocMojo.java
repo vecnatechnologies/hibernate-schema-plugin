a9027da4b8e346c603606c6e73b3f20d2c70600b
Oleg Golberg <ogolberg@vecna.com>
2012-06-26 14:53:48 -0400

    adding a Mojo that generates schema docs

---
+++ src/main/java/com/vecna/maven/hibernate/HibernateDocMojo.java
@@ -0,0 +1,71 @@
+/**
+ * NOTICE: This software  source code and any of  its derivatives are the
+ * confidential  and  proprietary   information  of  Vecna  Technologies,
+ * Inc. (such source  and its derivatives are hereinafter  referred to as
+ * "Confidential Information"). The  Confidential Information is intended
+ * to be  used exclusively by  individuals or entities that  have entered
+ * into either  a non-disclosure agreement or license  agreement (or both
+ * of  these agreements,  if  applicable) with  Vecna Technologies,  Inc.
+ * ("Vecna")   regarding  the  use   of  the   Confidential  Information.
+ * Furthermore,  the  Confidential  Information  shall be  used  only  in
+ * accordance  with   the  terms   of  such  license   or  non-disclosure
+ * agreements.   All  parties using  the  Confidential Information  shall
+ * verify that their  intended use of the Confidential  Information is in
+ * compliance  with and  not in  violation of  any applicable  license or
+ * non-disclosure  agreements.  Unless expressly  authorized by  Vecna in
+ * writing, the Confidential Information  shall not be printed, retained,
+ * copied, or  otherwise disseminated,  in part or  whole.  Additionally,
+ * any party using the Confidential  Information shall be held liable for
+ * any and  all damages incurred  by Vecna due  to any disclosure  of the
+ * Confidential  Information (including  accidental disclosure).   In the
+ * event that  the applicable  non-disclosure or license  agreements with
+ * Vecna  have  expired, or  if  none  currently  exists, all  copies  of
+ * Confidential Information in your  possession, whether in electronic or
+ * printed  form, shall be  destroyed or  returned to  Vecna immediately.
+ * Vecna  makes no  representations  or warranties  hereby regarding  the
+ * suitability  of  the   Confidential  Information,  either  express  or
+ * implied,  including  but not  limited  to  the  implied warranties  of
+ * merchantability,    fitness    for    a   particular    purpose,    or
+ * non-infringement. Vecna  shall not be liable for  any damages suffered
+ * by  licensee as  a result  of  using, modifying  or distributing  this
+ * Confidential Information.  Please email [info@vecnatech.com]  with any
+ * questions regarding the use of the Confidential Information.
+ */
+
+package com.vecna.maven.hibernate;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.plexus.util.FileUtils;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.hbm2x.DocExporter;
+
+/**
+ * Generates schema documentation
+ * @requiresDependencyResolution
+ * @phase compile
+ * @goal doc
+ * @author ogolberg@vecna.com
+ */
+public class HibernateDocMojo extends HibernateSchemaMojo {
+  /**
+   * @parameter
+   */
+  private File outputDir;
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected void executeWithMappings(Configuration configuration) throws MojoExecutionException, MojoFailureException {
+    try {
+      FileUtils.forceMkdir(outputDir);
+    } catch (IOException e) {
+      throw new MojoExecutionException("cannot create output directory " + outputDir, e);
+    }
+    new DocExporter(configuration, outputDir);
+  }
+}
